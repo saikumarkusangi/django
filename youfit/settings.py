8c3635d4f939a419d4b26558ab692bc0f7cc554e
Sai Kumar Kusangi <93697313+saikumarkusangi@users.noreply.github.com>
2023-05-08 23:33:45 +0530

    Update settings.py

--- youfit/settings.py
+++ youfit/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import urllib 
-
+import io
 from pathlib import Path
 from django.contrib.messages import constants as messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
