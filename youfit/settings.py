46eead6b46d73b0e2226d8cb699f245ec5ac7b5a
Sai Kumar Kusangi <93697313+saikumarkusangi@users.noreply.github.com>
2023-05-08 23:26:52 +0530

    Update settings.py

--- youfit/settings.py
+++ youfit/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'youfit.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-        'default': {
-            'ENGINE': 'djongo',
-            'NAME': 'Cluster0',
-            'ENFORCE_SCHEMA': False,
-            'CLIENT': {
-                'host': 'mongodb+srv://thekusangi007:'+ urllib.parse.quote("thekusangi007") +'@cluster0.ktvtukt.mongodb.net/?retryWrites=true&w=majority'
-            }  
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'djongo',
+#             'NAME': 'Cluster0',
+#             'ENFORCE_SCHEMA': False,
+#             'CLIENT': {
+#                 'host': 'mongodb+srv://thekusangi007:'+ urllib.parse.quote("thekusangi007") +'@cluster0.ktvtukt.mongodb.net/?retryWrites=true&w=majority'
+#             }  
+#         }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
